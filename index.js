a4b99b8958e2a850b89577c4ee189450c2ec34db
alsafa012 <alsafa012@gmail.com>
2023-11-06 18:03:54 +0600

    delete methode functionality added

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
                const result = await purchasedFoodCollection.insertOne(allData);
                res.send(result);
           });
+          
+          app.delete("/purchasedFoods/:id", async (req, res) => {
+               const id = req.params.id;
+               const query = { _id: new ObjectId(id) };
+               const result = await purchasedFoodCollection.deleteOne(query);
+               res.send(result);
+          });
 
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
